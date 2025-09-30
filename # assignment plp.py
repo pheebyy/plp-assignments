8201ebe526f6f7333df8fcc2df3c629dd59c2bad
Phoebe .C.A. Nyakiamo <118109619+pheebyy@users.noreply.github.com>
2025-09-30 08:14:46 +0300

    Add files via upload
    plp second assignment

---
+++ # assignment plp.py
@@ -0,0 +1,24 @@
+ # assignment plp
+my_list = []
+
+# Append elements 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+
+my_list.insert(1, 15)
+
+my_list.extend([50, 60, 70])
+
+
+my_list.pop()
+
+#
+my_list.sort()
+
+# Find and print the index of value 30
+index_of_30 = my_list.index(30)
+print("Final list:", my_list)
+print("Index of 30:", index_of_30)
